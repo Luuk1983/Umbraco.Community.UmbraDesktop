febc895e2bfd9613ffeb1e35da447dda3a580f4a
Luuk Peters <Luuk.Peters@proudnerds.com>
2026-08-09 23:04:07 +0200

    chore: drop empty localization scaffolds
    public/localization/{en,nl}.js were empty (export default {}) but still
    registered as extensions in umbraco-package.json. The real umbraDesktop
    strings live in src/desktop/localization/ and ship via the bundle.

--- src/Umbraco.Community.UmbraDesktop/backoffice/public/localization/en.js
+++
@@ -1,6 +0,0 @@
-﻿//Register your backoffice localization here for English
-//https://docs.umbraco.com/umbraco-cms/customizing/extending-overview/extension-types/localization
-
-export default {
-};
-
--- src/Umbraco.Community.UmbraDesktop/backoffice/public/localization/nl.js
+++
@@ -1,5 +0,0 @@
-﻿//Register your backoffice localization here for Dutch
-//https://docs.umbraco.com/umbraco-cms/customizing/extending-overview/extension-types/localization
-
-export default {
-};
